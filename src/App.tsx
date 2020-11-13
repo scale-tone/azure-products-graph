856a46f1159ac8811dd36a4a6a5ff8d51fe31588
Konstantin Lepeshenkov <kolepes@microsoft.com>
2020-11-13 21:17:03 +0100

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import { observer } from 'mobx-react';
+import styled from 'styled-components';
 
+import { AppBar, Box, Button, Link, Toolbar, Typography } from '@material-ui/core';
+
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+import logo from './logo.svg';
+
+@observer
 export default class App extends React.Component {
 
     render(): JSX.Element {
 
-        const uri = `${process.env.REACT_APP_BACKEND_BASE_URI}/HttpTest`;
+        return (<>
+            <AppBar position="static" color="default">
+                <Toolbar>
+                    <Typography variant="h4" color="inherit">
+                        Azure Products Graph
+                    </Typography>
+                </Toolbar>
+            </AppBar>
 
-        return (<button onClick={() => {
-            fetch(uri)
-                .then(response => response.text())
-                .then(text => alert(text))
-
-        }}>
-            Press me to call {uri}
-        </button>);
+            <BottomBar position="fixed" color="transparent" ><Toolbar variant="dense">
+                <Typography style={{ flex: 1 }} />
+                <Button startIcon={<GitHubIcon />}
+                    href="https://github.com/scale-tone/azure-products-graph"
+                    target="_blank"
+                >
+                    Fork me on GitHub
+                    </Button>
+            </Toolbar></BottomBar>
+        </>);
     }
 }
+
+const BottomBar: typeof AppBar = styled(AppBar)({
+    top: 'auto',
+    bottom: 0
+})
