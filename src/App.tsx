0a7fd760ad42ff1dae053cc8dee599264362f200
Konstantin Lepeshenkov <kolepes@microsoft.com>
2020-11-14 18:24:41 +0100

    stuff implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,143 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import styled from 'styled-components';
 
-import { AppBar, Box, Button, Link, Toolbar, Typography } from '@material-ui/core';
-
-import GitHubIcon from '@material-ui/icons/GitHub';
+import { AppBar, Box, Button, Menu, MenuItem, Slider, TextField, Toolbar, Typography } from '@material-ui/core';
+import { Graph } from "react-d3-graph";
 
 import logo from './logo.svg';
+import { AppState } from './AppState';
 
 @observer
-export default class App extends React.Component {
+export default class App extends React.Component<{ state: AppState }> {
+
+    componentDidMount() {
+        this.props.state.load();
+    }
 
     render(): JSX.Element {
+        const state = this.props.state;
 
         return (<>
             <AppBar position="static" color="default">
                 <Toolbar>
-                    <Typography variant="h4" color="inherit">
+
+                    <img src={logo} width="30px" alt="" />
+                    <Box width={30} />
+
+                    <TitleTypography variant="h5" color="inherit">
                         Azure Products Graph
-                    </Typography>
+                    </TitleTypography>
+
+                    <ZoomSlider value={state.zoomLevel} min={0.5} max={5} step={0.1} 
+                        onChange={(evt, newValue) => {state.zoomLevel = newValue as number}}
+                    />
+
+                    <FilterTextField
+                        margin="dense"
+                        label="Filter by"
+                        InputLabelProps={{ shrink: true }}
+                        placeholder="e.g. 'sql'"
+                        disabled={state.inProgress}
+                        value={state.filterText}
+                        onChange={(evt) => state.filterText = evt.target.value as string}
+                        onKeyPress={(evt: React.KeyboardEvent<HTMLInputElement>) => this.handleKeyPress(evt)}
+                    />
+
+                    <FilterButton
+                        variant="outlined"
+                        color="default"
+                        size="large"
+                        disabled={state.inProgress}
+                        onClick={() => state.load()}
+                    >
+                        Filter
+                    </FilterButton>
                 </Toolbar>
             </AppBar>
 
-            <BottomBar position="fixed" color="transparent" ><Toolbar variant="dense">
-                <Typography style={{ flex: 1 }} />
-                <Button startIcon={<GitHubIcon />}
-                    href="https://github.com/scale-tone/azure-products-graph"
-                    target="_blank"
-                >
-                    Fork me on GitHub
-                    </Button>
-            </Toolbar></BottomBar>
+            <Menu
+                anchorEl={state.popupAnchorElement}
+                keepMounted
+                open={!!state.popupAnchorElement}
+                onClose={() => state.popupAnchorElement = undefined}
+            >
+                {state.selectedProducts.map(product => {
+                    return (
+                        <MenuItem key={product.title} onClick={() => state.onProductSelected(product)}>{product.title}</MenuItem>
+                    )
+                })}
+
+            </Menu>
+
+            {!state.inProgress && state.nodes.length > 0 && state.links.length > 0 && (
+                <Graph
+                    id="graph-id"
+                    data={{ nodes: state.nodes, links: state.links }}
+
+                    config={{
+
+                        height: window.innerHeight - 100,
+                        width: window.innerWidth,
+                        initialZoom: state.zoomLevel,
+
+                        node: {
+                            color: "#50E6FF",
+                            labelPosition: 'center',
+                        },
+
+                        link: {
+                            renderLabel: true
+                        },
+
+                        d3: {
+                            linkLength: 200,
+                            gravity: -70
+                        }
+                    }}
+
+                    onClickLink={(source, target) => state.onLinkSelected(source, target)}
+                    onClickNode={(keyword) => state.onNodeSelected(keyword)}
+                />
+            )}
+
+            <GithubLink href="https://github.com/scale-tone/azure-products-graph" target="_blank">
+                <img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149" alt="Fork me on GitHub" data-recalc-dims="1" />
+            </GithubLink>
+
         </>);
+    }
+
+    private handleKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
+        if (event.key === 'Enter') {
+            // Otherwise the event will bubble up and the form will be submitted
+            event.preventDefault();
+
+            this.props.state.load();
+        }
     }
 }
 
-const BottomBar: typeof AppBar = styled(AppBar)({
-    top: 'auto',
-    bottom: 0
-})
+const TitleTypography: typeof Typography = styled(Typography)({
+    minWidth: 280
+});
+
+const ZoomSlider: typeof Slider = styled(Slider)({
+    minWidth: 100,
+    marginRight: 50
+});
+
+const FilterTextField: typeof TextField = styled(TextField)({
+    minWidth: 200,
+    marginRight: 20
+});
+
+const FilterButton: typeof Button = styled(Button)({
+    minWidth: 100,
+    marginRight: 120
+});
+
+const GithubLink = styled.a({
+    position: 'absolute',
+    right: 0,
+    top: 0
+});
---
+++ src/AppState.ts
@@ -0,0 +1,251 @@
+import { observable, computed } from 'mobx';
+import axios from 'axios';
+
+export class AppState {
+
+    @observable
+    zoomLevel: number = 1;
+
+    @observable
+    popupAnchorElement?: Element;
+
+    @observable
+    filterText: string = "";
+
+    @computed
+    get inProgress(): boolean { return this._inProgress; };
+
+    @computed
+    get selectedProducts(): IProduct[] { return this._selectedProducts };
+
+    @computed
+    get nodes(): INode[] { return this._nodes; }
+    @computed
+    get links(): ILink[] { return this._links; }
+
+    load() {
+
+        if (this._inProgress) {
+            return;
+        }
+        this._inProgress = true;
+
+        this._nodes = [];
+        this._links = [];
+
+        this.getProducts().then(products => {
+
+            const nodes: INode[] = [];
+            const links: ILink[] = [];
+
+            for (var product of products) {
+
+                const keywords = this.getKeywordsFromTitle(product.title);
+
+                if (keywords.length === 1) {
+
+                    // Adding product as a node (since the graph component is unable to show self-referencing links)
+                    nodes.push({ id: product.title, size: 200, color: 'lightgrey' });
+                    links.push({ source: product.title, target: product.title, label: product.title })
+                    
+                    this.addProductToMap(product.title, product);
+
+                } else {
+
+                    var prevKeyword = '';
+                    for (var keyword of keywords) {
+
+                        const node = nodes.find(n => n.id === keyword);
+                        if (!node) {
+                            nodes.push({ id: keyword, size: 200 });
+                        } else {
+                            node.size += 200;
+                        }
+
+                        if (!!prevKeyword) {
+                            const link = links.find(l =>
+                                (l.source === prevKeyword && l.target === keyword) ||
+                                (l.target === prevKeyword && l.source === keyword)
+                            );
+
+                            if (!link) {
+                                links.push({ source: prevKeyword, target: keyword, label: product.title })
+                            }
+                        }
+                        prevKeyword = keyword;
+
+                        this.addProductToMap(keyword, product);
+                    }
+                }
+
+            }
+
+            this._nodes = nodes;
+            this._links = links;
+
+        }).finally(() => {
+            this._inProgress = false;
+        });
+    }
+
+    onNodeSelected(keyword: string): void {
+
+        if (!!this._productsMap[keyword]) {
+
+            this._selectedProducts = this._productsMap[keyword]
+                .filter((p1, index, self) => self.findIndex(p2 => p2.title === p1.title) === index);
+
+        } else {
+
+            this._selectedProducts = [];
+        }
+
+        const nodeElements = document.querySelectorAll(`[id="${keyword}"]`);
+        if (nodeElements.length > 0) {
+            this.popupAnchorElement = nodeElements[0];
+        }
+
+        // Automatically hiding menu after N sec.
+        setTimeout(() => { this.popupAnchorElement = undefined }, PopupTimeout);
+    }
+
+    onLinkSelected(keyword1: string, keyword2: string): void {
+
+        // Taking an intersection of products
+        const products1 = this._productsMap[keyword1] ?? [];
+        const products2 = this._productsMap[keyword2] ?? [];
+        const selectedProducts = products1
+            .filter(p1 => products2.some(p2 => p2.title === p1.title))
+            .filter((p1, index, self) => self.findIndex(p2 => p2.title === p1.title) === index);
+
+        this._selectedProducts = selectedProducts;
+        if (selectedProducts.length <= 0) {
+            return;
+        }
+
+        var linkElements = document.querySelectorAll(`[href="#${keyword1},${keyword2}"]`);
+        if (linkElements.length > 0) {
+            this.popupAnchorElement = linkElements[0];
+        } else {
+
+            linkElements = document.querySelectorAll(`[href="#${keyword2},${keyword1}"]`);
+            if (linkElements.length > 0) {
+                this.popupAnchorElement = linkElements[0];
+            }
+        }
+
+        // Automatically hiding menu after N sec.
+        setTimeout(() => { this.popupAnchorElement = undefined }, PopupTimeout);
+    }
+
+    onProductSelected(product: IProduct): void {
+
+        window.open(`https://azure.microsoft.com${product.url}`, '_blank');
+    }
+
+    @observable
+    private _nodes: INode[] = [];
+    @observable
+    private _links: ILink[] = [];
+    @observable
+    private _selectedProducts: IProduct[] = [];
+    @observable
+    private _inProgress: boolean;
+
+    private _productsMap: { [keyword: string]: IProduct[] } = {};
+
+    private addProductToMap(keyword: string, product: IProduct) {
+
+        if (!this._productsMap[keyword]) {
+            this._productsMap[keyword] = [];
+        }
+        this._productsMap[keyword].push(product);
+    }
+
+    private getProducts(): Promise<IProduct[]>{
+
+        return axios.get(`${process.env.REACT_APP_BACKEND_BASE_URI}/services-html`).then(response => {
+
+            const html = response.data;
+
+            const regex = /<a href="(\/[^"]+)" data-event="area-products-index-clicked-product" data-event-property="([^"]+)">/gi;
+
+            const filterText = this.filterText.toLowerCase();
+            const result: { url: string, title: string }[] = [];
+            var match: RegExpExecArray | null;
+            while (!!(match = regex.exec(html))) {
+
+                const url = match[1];
+                const title = match[2];
+
+                if (!!this.filterText && !title.toLowerCase().includes(filterText)) {
+                    continue;
+                }
+
+                // making result distinct
+                if (!result.some(s => s.url === url)) {
+                    result.push({ url, title });
+                }
+            }
+
+            return result;
+        });
+    }
+
+    private getKeywordsFromTitle(title: string): string[]{
+
+        return title.split(' ').map(w => w.toUpperCase()).filter(w =>
+            !!w &&
+            !!w.trim() &&
+            !w.startsWith('(') &&
+            !WordsToExclude.includes(w)
+        ).map(w => Mappings[w] ?? w);
+    }
+}
+
+const PopupTimeout = 8000;
+
+const WordsToExclude = [
+    "MICROSOFT",
+    "AZURE",
+    "WINDOWS",
+    "AND",
+    "OF",
+    "ON",
+    "FOR",
+    "TO",
+    "+",
+    "10"
+];
+
+const Mappings = {
+    "APP": "APPLICATION",
+    "APPS": "APPLICATION",
+    "APPLICATIONS": "APPLICATION",
+    "CONTAINERS": "CONTAINER",
+    "DESKTOPS": "DESKTOP",
+    "HUBS": "HUB",
+    "INSTANCES": "INSTANCE",
+    "LABS": "LAB",
+    "MACHINES": "MACHINE",
+    "SERVICES": "SERVICE",
+    "STREAMING": "STREAM",
+    "TRANSLATOR": "TRANSLATION"
+};
+
+interface INode {
+    id: string;
+    size: number;
+    color?: string;
+}
+
+interface ILink {
+    source: string;
+    target: string;
+    label: string;
+}
+
+interface IProduct {
+    url: string;
+    title: string;
+}
